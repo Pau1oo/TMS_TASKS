8054d079faae4e97ca756412fe1f736f669b8f30
Pau1oo <bukato_pavel@mail.ru>
2024-06-29 15:07:32 +0300

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя: ");
+string name = Console.ReadLine();
+Console.Write("Введите фамилию: ");
+string surname = Console.ReadLine();
+Console.Write("Введите отчество: ");
+string fname = Console.ReadLine();
+
+string[] keys = { surname, name, fname };
+
+string fullName = string.Join(" ", keys);
+Console.WriteLine("ФИО: " + fullName);
+
+Console.ReadKey();
