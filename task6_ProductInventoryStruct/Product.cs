9a753653d36e069910e03158692f7e9899638932
Pau1oo <bukato_pavel@mail.ru>
2024-07-17 23:05:41 +0300

    struct realisation

---
+++ task6_ProductInventoryStruct/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6_ProductInventoryStruct
+{
+    public struct Product
+    {
+        public string ID;
+        public decimal Price;
+        public int Quantity;
+
+        public Product(string ID, decimal Price, int Quantity)
+        {
+            this.ID = ID;
+            this.Price = Price;
+            this.Quantity = Quantity;
+        }
+        public Product(string ID) : this(ID, 0, 0) { }
+        public Product(decimal Price) : this("Неизвестно", Price, 0) { }
+        public Product(int Quantity) : this("Неизвестно", 0, Quantity) { }
+        public Product(string ID, decimal Price) : this(ID, Price, 0) { }
+        public Product(string ID, int Quantity) : this(ID, 0, Quantity) { }
+        public Product(decimal Price, int Quantity) : this("Неизвестно", Price, Quantity) { }
+        public Product() : this("Неизвестно", 0, 0) { }
+    }
+}
+
--- task6_ProductInventoryStruct/Program.cs
+++ task6_ProductInventoryStruct/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System.Threading.Channels;
+using task6_ProductInventoryStruct;
+
+class Program
+{
+    public static void Main()
+    {
+        Product prod1 = new Product(5m);
+        Product prod2 = new Product("Молоко", 6m, 3);
+        Product prod3 = new Product("Банан", 2);
+        Product prod4 = new Product("Сыр", 3m);
+        Product prod5 = new Product();
+        Product prod6 = new Product(1, 3);
+        Product prod7 = new Product("Апельсин", 7m, 10);
+        Product prod8 = new Product("Яблоко", 3m, 6);
+
+        Inventory inv = new Inventory();
+
+        Inventory.AddItem(ref inv, prod1);
+        Inventory.AddItem(ref inv, prod2);
+        Inventory.AddItem(ref inv, prod3);
+        Inventory.AddItem(ref inv, prod4);
+        Inventory.AddItem(ref inv, prod5);
+        Inventory.AddItem(ref inv, prod6);
+        Inventory.AddItem(ref inv, prod7);
+        Inventory.AddItem(ref inv, prod8);
+
+        Inventory.ShowInventoryInfo(inv);
+        Console.WriteLine("\n\n");
+
+        Inventory.RemoveItem(ref inv, prod5);
+        Inventory.RemoveItem(ref inv, prod1);
+        Inventory.RemoveItem(ref inv, prod8);
+
+        Inventory.ShowInventoryInfo(inv);
+        Console.ReadKey();
+    }
+}
