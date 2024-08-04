0c0801b53d99a8fc301040c0289844c3c77bf43f
Pau1oo <bukato_pavel@mail.ru>
2024-08-04 20:10:54 +0300

    add class ComparablePair, test

---
+++ task10/ComparablePair.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10
+{
+    class ComparablePair<T, U> : IComparable<ComparablePair<T, U>>
+        where T : IComparable<T>
+        where U : IComparable<U>
+    {
+        public T t_value;
+        public U u_value;
+
+        public T T_Value 
+        {   
+            get { return t_value; } 
+            set { t_value = value; }
+        }
+        public U U_Value 
+        { 
+            get { return u_value; } 
+            set { u_value = value; } 
+        }
+
+        public ComparablePair(T T_Value, U U_Value)
+        {
+            t_value = T_Value;
+            u_value = U_Value;
+        }
+
+        public int CompareTo(ComparablePair<T, U> other)
+        {
+            if (other == null)
+                return 1;
+
+            int tComparison = t_value.CompareTo(other.t_value);
+            if (tComparison != 0)
+                return tComparison;
+
+            return u_value.CompareTo(other.u_value);
+        }
+    }
+}
---
+++ task10/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace task10
+{
+    class Program
+    {
+        public static int input()
+        {
+            int num;
+            Console.WriteLine("Введите число:");
+            while (!int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.Clear();
+                Console.WriteLine("Ошибка! Нажмите любую кнопку...");
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Введите число:");
+            }
+            Console.Clear();
+            return num;
+        }
+        static void Main()
+        {
+            ComparablePair<int, int> pair1 = new ComparablePair<int, int>(input(), input());
+            ComparablePair<int, int> pair2 = new ComparablePair<int, int>(input(), input());
+
+            Console.WriteLine($"Первая пара - {pair1.t_value}, {pair1.u_value}\n" +
+                              $"Вторая пара - {pair2.t_value}, {pair2.u_value}");
+
+            if (pair1.CompareTo(pair2) == 0 ) 
+            {
+                Console.WriteLine("Пары равны!");
+            }
+            else if(pair1.CompareTo(pair2) == 1)
+            {
+                Console.WriteLine("Первая пара больше!");
+            }
+            else if (pair1.CompareTo(pair2) == -1)
+            {
+                Console.WriteLine("Вторая пара больше!");
+            }
+        } 
+    }
+}
