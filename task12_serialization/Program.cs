d4a6c9fe315f646f5278c5b078ec8177c91d8d4d
Pau1oo <bukato_pavel@mail.ru>
2024-08-05 20:30:08 +0300

    initial commit

---
+++ task12_serialization/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace task12_serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
